a8c13007f0b5b27963df6e3f1edb783ac8f223b1
Maria Shodunke (The Boss Lady) <mariashow2001@gmail.com>
2017-01-15 23:27:04 +0000

    Added styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const Header = () => {
+	const { textStyle, viewStyle } = styles;
+	
 	return (
-		<Text>Albums!</Text>
+		<View style={viewStyle}>
+			<Text style={textStyle}>Albums!</Text>
+		</View>
 	);
+};
+
+const styles = {
+	textStyle: {
+		fontSize: 20
+	},
+	viewStyle: {
+		backgroundColor: '#F8F8F8' 
+	}
 };
 
 export default Header;
